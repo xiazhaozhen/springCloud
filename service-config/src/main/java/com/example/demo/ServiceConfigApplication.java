e6e1dd0b3e3a73c5476ef9b1cda47271fd3a56ef
夏肇真 <422645940@qq.com>
2018-04-12 13:46:40 +0800

    分布式配置中心

--- service-config/src/main/java/com/example/demo/ServiceConfigApplication.java
+++ service-config/src/main/java/com/example/demo/ServiceConfigApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class ServiceConfigApplication {
 
 	public static void main(String[] args) {
